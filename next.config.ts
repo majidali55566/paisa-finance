ecbc3e34394c005def285adfc4291dd76ce623ca
Majid Ali <majidalipahore55566@gmail.com>
2025-05-26 00:37:14 +0500

    updated type alliases

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   images: {
@@ -6,6 +7,14 @@ const nextConfig: NextConfig = {
   },
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "@": path.resolve(__dirname, "src"),
+      "@ui": path.resolve(__dirname, "src/components/ui"),
+    };
+    return config;
   },
 };
 
