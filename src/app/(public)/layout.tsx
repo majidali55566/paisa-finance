2df059b50cfd4cde42e6fcf13aff484545051dee
Majid Ali <majidalipahore55566@gmail.com>
2025-05-25 19:39:45 +0500

    deploying

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -8,7 +8,7 @@ export default function PublicLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="">
+    <div>
       <PublicHeader />
       <main>{children}</main>
       <Toaster richColors />
